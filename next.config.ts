ede891185d6eb4433c1fc4062366a502104ab698
Francisco Madeira <francisco.madeira@diconium.com>
2025-03-06 19:48:23 +0000

    feat: first blood

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,24 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  publicRuntimeConfig: {
+    // Will be available on both server and client
+    CONTENTSTACK_API_KEY: process.env.CONTENTSTACK_API_KEY,
+    CONTENTSTACK_DELIVERY_TOKEN: process.env.CONTENTSTACK_DELIVERY_TOKEN,
+    CONTENTSTACK_BRANCH: process.env.CONTENTSTACK_BRANCH || "main",
+    CONTENTSTACK_REGION: process.env.CONTENTSTACK_REGION || "us",
+    CONTENTSTACK_ENVIRONMENT: process.env.CONTENTSTACK_ENVIRONMENT,
+    CONTENTSTACK_PREVIEW_TOKEN: process.env.CONTENTSTACK_PREVIEW_TOKEN,
+    CONTENTSTACK_PREVIEW_HOST:
+      process.env.CONTENTSTACK_PREVIEW_HOST || "rest-preview.contentstack.com",
+    CONTENTSTACK_API_HOST:
+      process.env.CONTENTSTACK_API_HOST || "api.contentstack.io",
+    CONTENTSTACK_APP_HOST:
+      process.env.CONTENTSTACK_APP_HOST || "app.contentstack.com",
+    CONTENTSTACK_LIVE_PREVIEW: process.env.CONTENTSTACK_LIVE_PREVIEW || "true",
+    CONTENTSTACK_LIVE_EDIT_TAGS:
+      process.env.CONTENTSTACK_LIVE_EDIT_TAGS || "false",
+  },
 };
 
 export default nextConfig;
